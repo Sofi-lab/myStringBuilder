e136156021bdd28afbe72526621dbc29cbb6687f
Sofi <sofa.kirikova@yandex.ru>
2024-05-22 19:06:18 +0300

    Написан мой класс для StringBuilder

--- myStringBuilder/src/main/java/com/example/myStringBuilder/MyStringBuilder.java
+++ myStringBuilder/src/main/java/com/example/myStringBuilder/MyStringBuilder.java
@@ -1,4 +1,53 @@
 package com.example.myStringBuilder;
 
+import lombok.Getter;
+
+import java.util.Stack;
+
+@Getter
 public class MyStringBuilder {
+
+    private Stack<String> stack;
+    private StringBuilder builder;
+
+    public MyStringBuilder() {
+        this.builder = new StringBuilder();
+        this.stack = new Stack<>();
+    }
+
+    public MyStringBuilder append(String str) {
+        saveStack(str);
+        builder.append(str);
+        return this;
+    }
+
+    private void saveStack(String string) {
+        stack.push(string);
+    }
+
+    public void undo() {
+        if (stack.size() > 1) {
+            stack.pop();
+            String previousPos = stack.peek();
+            builder = new StringBuilder(previousPos);
+        } else {
+            throw new RuntimeException("This is start state");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        MyStringBuilder build = new MyStringBuilder();
+        build.append(" firstAdd,");
+        build.append(" secondAdd,");
+
+        build.undo();
+        build.append(" third append");
+
+        System.out.println(build);
+    }
 }
